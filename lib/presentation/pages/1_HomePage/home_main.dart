241b94c6a6632f63f9d0937bafb8e6c117dca18c
suho <chooshow@gmail.com>
2022-08-30 12:11:58 +0900

    20220830 AnimationButton -> ValueNotifierBuiler 사용

--- lib/presentation/pages/1_HomePage/home_main.dart
+++ lib/presentation/pages/1_HomePage/home_main.dart
@@ -13,18 +13,15 @@ class HomeMain extends StatefulWidget {
 
 class _HomeMainState extends State<HomeMain> {
   late final ScrollController _scrollController;
+  final ValueNotifier<double> _opacityNotifier = ValueNotifier<double>(0);
   double opacity = 0;
   bool _isSelected = false;
 
   void _opacityFAB() {
     if (_scrollController.offset <= 50) {
-      setState(() {
-        opacity = 0;
-      });
+      _opacityNotifier.value = 0;
     } else {
-      setState(() {
-        opacity = 1;
-      });
+      _opacityNotifier.value = 1;
     }
   }
 
@@ -55,56 +52,91 @@ class _HomeMainState extends State<HomeMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebSmoothScroll(
-        controller: _scrollController,
-        curve: Curves.fastOutSlowIn,
-        animationDuration: 300,
-        child: SingleChildScrollView(
+        body: WebSmoothScroll(
           controller: _scrollController,
-          child: Column(
-            children: buildContents(),
+          curve: Curves.fastOutSlowIn,
+          animationDuration: 300,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              children: buildContents(),
+            ),
           ),
         ),
-      ),
-      floatingActionButton: AnimatedPadding(
-        duration: const Duration(milliseconds: 60),
-        padding: EdgeInsets.only(bottom: _isSelected ? 10 : 5),
-        child: AnimatedOpacity(
-          opacity: opacity,
-          duration: const Duration(milliseconds: 300),
-          child: MaterialButton(
-            hoverColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            onPressed: () {
-              _scrollController.animateTo(0,
-                  duration: const Duration(milliseconds: 500), curve: Curves.linear);
+        floatingActionButton: AnimatedPadding(
+          duration: const Duration(milliseconds: 60),
+          padding: EdgeInsets.only(bottom: _isSelected ? 10 : 5),
+          child: MouseRegion(
+            onEnter: (event) {
+              setState(() {
+                _isSelected = true;
+              });
             },
-            child: MouseRegion(
-              onEnter: (event) {
-                setState(() {
-                  _isSelected = true;
-                });
-              },
-              onExit: (event) {
-                setState(() {
-                  _isSelected = false;
-                });
-              },
-              child: const PhysicalModel(
-                color: Colors.black,
-                elevation: 15.0,
-                shape: BoxShape.circle,
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 28,
-                  child: Icon(
-                    Icons.keyboard_arrow_up,
-                    color: Colors.black54,
-                    size: 40,
-                  ),
-                ),
+            onExit: (event) {
+              setState(() {
+                _isSelected = false;
+              });
+            },
+            child: ValueListenableBuilder(
+              valueListenable: _opacityNotifier,
+              builder: (BuildContext context, double opacity, Widget? child) => AnimatedFloatingActionButton(
+                function: () {
+                  print('clicked');
+                  _scrollController.animateTo(0,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.linear);
+                },
+                opacity: opacity
               ),
+            ),
+          ),
+        ));
+  }
+}
+
+class AnimatedFloatingActionButton extends StatefulWidget {
+  const AnimatedFloatingActionButton(
+      {Key? key,
+      required this.opacity,
+      required this.function,
+      })
+      : super(key: key);
+  final double opacity;
+  final GestureTapCallback function;
+
+  @override
+  State<AnimatedFloatingActionButton> createState() =>
+      _AnimatedFloatingActionButtonState();
+}
+
+class _AnimatedFloatingActionButtonState
+    extends State<AnimatedFloatingActionButton> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: widget.function,
+      child: AnimatedOpacity(
+        opacity: widget.opacity,
+        duration: const Duration(milliseconds: 300),
+        child: const PhysicalModel(
+          color: Colors.black,
+          elevation: 15.0,
+          shape: BoxShape.circle,
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 28,
+            child: Icon(
+              Icons.keyboard_arrow_up,
+              color: Colors.black54,
+              size: 40,
             ),
           ),
         ),
@@ -112,3 +144,40 @@ class _HomeMainState extends State<HomeMain> {
     );
   }
 }
+
+// MaterialButton(
+// hoverColor: Colors.transparent,
+// splashColor: Colors.transparent,
+// highlightColor: Colors.transparent,
+// onPressed: () {
+// _scrollController.animateTo(0,
+// duration: const Duration(milliseconds: 500),
+// curve: Curves.linear);
+// },
+// child: MouseRegion(
+// onEnter: (event) {
+// setState(() {
+// _isSelected = true;
+// });
+// },
+// onExit: (event) {
+// setState(() {
+// _isSelected = false;
+// });
+// },
+// child: const PhysicalModel(
+// color: Colors.black,
+// elevation: 15.0,
+// shape: BoxShape.circle,
+// child: CircleAvatar(
+// backgroundColor: Colors.white,
+// radius: 28,
+// child: Icon(
+// Icons.keyboard_arrow_up,
+// color: Colors.black54,
+// size: 40,
+// ),
+// ),
+// ),
+// ),
+// ),
